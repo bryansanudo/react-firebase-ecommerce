64528f5782d3e4c26d822b6da131cc10835055bd
Bryan <bryansanudo95@gmail.com>
2023-05-23 12:35:57 -0500

    delete product

--- src/components/admin/addProduct/AddProduct.jsx
+++ src/components/admin/addProduct/AddProduct.jsx
@@ -1,9 +1,12 @@
 import styles from "@/components/admin/addProduct/AddProduct.module.scss";
 import Card from "@/components/card/Card";
 import { useState } from "react";
-import { storage } from "@/firebase/config";
+import { storage, db } from "@/firebase/config";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { toast } from "react-toastify";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import Loader from "@/components/loader/Loader";
+import { useNavigate } from "react-router-dom";
 
 const categories = [
   {
@@ -24,17 +27,24 @@ const categories = [
   },
 ];
 
+const initialState = {
+  name: "",
+  imageURL: "",
+  price: 0,
+  category: "",
+  brand: "",
+  desc: "",
+};
+
 const AddProduct = () => {
   const [product, setProduct] = useState({
-    name: "",
-    imageURL: "",
-    price: 0,
-    category: "",
-    brand: "",
-    desc: "",
+    ...initialState,
   });
 
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -67,107 +77,131 @@ const AddProduct = () => {
 
   const addProduct = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     console.log(product);
+
+    try {
+      const docRef = addDoc(collection(db, "products"), {
+        name: product.name,
+        imageURL: product.imageURL,
+        price: Number(product.price),
+        category: product.category,
+        brand: product.brand,
+        desc: product.desc,
+        createdAt: Timestamp.now().toDate(),
+      });
+      setIsLoading(false);
+      setUploadProgress(0);
+      setProduct({ ...initialState });
+      navigate("/admin/all-products");
+      toast.success("Product uploaded sucessfully.");
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
   };
 
   return (
-    <div className={styles.product}>
-      <h1>Add New Product</h1>
-      <Card className={styles.card}>
-        <form onSubmit={addProduct}>
-          <label>Product name:</label>
-          <input
-            type="text"
-            placeholder="Product name"
-            name="name"
-            required
-            value={product.name}
-            onChange={(e) => handleInputChange(e)}
-          />
-          <label>Product image:</label>
-          <Card className={styles.group}>
-            {uploadProgress === 0 ? null : (
-              <div className={styles.progress}>
-                <div
-                  className={styles["progress-bar"]}
-                  style={{ width: `${uploadProgress}%` }}
-                >
-                  {uploadProgress < 100
-                    ? `Uploading ${uploadProgress}`
-                    : `Upload Complete ${uploadProgress}%`}
-                </div>
-              </div>
-            )}
-
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.product}>
+        <h1>Add New Product</h1>
+        <Card className={styles.card}>
+          <form onSubmit={addProduct}>
+            <label>Product name:</label>
             <input
-              type="file"
-              accept="image/*"
-              placeholder="Product Image"
-              name="image"
-              onChange={(e) => handleImageChange(e)}
+              type="text"
+              placeholder="Product name"
+              name="name"
+              required
+              value={product.name}
+              onChange={(e) => handleInputChange(e)}
             />
+            <label>Product image:</label>
+            <Card className={styles.group}>
+              {uploadProgress === 0 ? null : (
+                <div className={styles.progress}>
+                  <div
+                    className={styles["progress-bar"]}
+                    style={{ width: `${uploadProgress}%` }}
+                  >
+                    {uploadProgress < 100
+                      ? `Uploading ${uploadProgress}`
+                      : `Upload Complete ${uploadProgress}%`}
+                  </div>
+                </div>
+              )}
 
-            {product.imageURL === "" ? null : (
               <input
-                type="text"
-                //required
-                placeholder="Image URL"
-                name="imageURL"
-                value={product.imageURL}
-                disabled
+                type="file"
+                accept="image/*"
+                placeholder="Product Image"
+                name="image"
+                onChange={(e) => handleImageChange(e)}
               />
-            )}
-          </Card>
 
-          <label>Product Price:</label>
-          <input
-            type="number"
-            placeholder="Product Price"
-            name="price"
-            required
-            value={product.price}
-            onChange={(e) => handleInputChange(e)}
-          />
-          <label>Product Category:</label>
-          <select
-            required
-            name="category"
-            value={product.category}
-            onChange={(e) => handleInputChange(e)}
-          >
-            <option value="" disabled>
-              -- choose product category --
-            </option>
-            {categories.map((cat) => {
-              return (
-                <option key={cat.id} value={cat.name}>
-                  {cat.name}
-                </option>
-              );
-            })}
-          </select>
-          <label>Product Company Brand:</label>
-          <input
-            type="text"
-            placeholder="Product Brand"
-            name="brand"
-            required
-            value={product.brand}
-            onChange={(e) => handleInputChange(e)}
-          />
-          <label>Product Description</label>
-          <textarea
-            required
-            name="desc"
-            value={product.desc}
-            onChange={(e) => handleInputChange(e)}
-            cols="30"
-            rows="10"
-          ></textarea>
-          <button className="--btn --btn-primary">Save Product</button>
-        </form>
-      </Card>
-    </div>
+              {product.imageURL === "" ? null : (
+                <input
+                  type="text"
+                  //required
+                  placeholder="Image URL"
+                  name="imageURL"
+                  value={product.imageURL}
+                  disabled
+                />
+              )}
+            </Card>
+
+            <label>Product Price:</label>
+            <input
+              type="number"
+              placeholder="Product Price"
+              name="price"
+              required
+              value={product.price}
+              onChange={(e) => handleInputChange(e)}
+            />
+            <label>Product Category:</label>
+            <select
+              required
+              name="category"
+              value={product.category}
+              onChange={(e) => handleInputChange(e)}
+            >
+              <option value="" disabled>
+                -- choose product category --
+              </option>
+              {categories.map((cat) => {
+                return (
+                  <option key={cat.id} value={cat.name}>
+                    {cat.name}
+                  </option>
+                );
+              })}
+            </select>
+            <label>Product Company Brand:</label>
+            <input
+              type="text"
+              placeholder="Product Brand"
+              name="brand"
+              required
+              value={product.brand}
+              onChange={(e) => handleInputChange(e)}
+            />
+            <label>Product Description</label>
+            <textarea
+              required
+              name="desc"
+              value={product.desc}
+              onChange={(e) => handleInputChange(e)}
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className="--btn --btn-primary">Save Product</button>
+          </form>
+        </Card>
+      </div>
+    </>
   );
 };
 
--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -1,7 +1,140 @@
-import React from "react";
+import styles from "@/components/admin/viewProducts/ViewProducts.module.scss";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { db, storage } from "@/firebase/config";
+import {
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import { Link } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Loader from "@/components/loader/Loader";
+import Notiflix from "notiflix";
 
 const ViewProducts = () => {
-  return <div>ViewProducts</div>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, "products");
+
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  const confirmDelete = (id, imageURL) => {
+    Notiflix.Confirm.show(
+      "Delete Product!!!",
+      "You are about to delete this product",
+      "Delete",
+      "Cancel",
+      function okCb() {
+        deleteProduct(id, imageURL);
+      },
+      function cancelCb() {
+        console.log("Delete Canceleded");
+      },
+      {
+        width: "320px",
+        borderRadius: "3px",
+        titleColor: "orangered",
+        okButtonBackground: "orangeRed",
+        cssAnimationStyle: "zoom",
+      }
+    );
+  };
+
+  const deleteProduct = async (id, imageURL) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+      const storageRef = ref(storage, imageURL);
+      deleteObject(storageRef);
+      toast.success("Product deleted successfully.");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.table}>
+        <h2>All Products</h2>
+        {products.length === 0 ? (
+          <p>No product found.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>s/n</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => {
+                const { id, name, price, imageURL, category } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`$${price}`}</td>
+                    <td className={styles.icons}>
+                      <Link to="/admin/add-product">
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <FaTrashAlt
+                        size={18}
+                        color="red"
+                        onClick={() => confirmDelete(id, imageURL)}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ViewProducts;
