53fd0c5da97dae1e6750959115d8dc1a70bbc1fe
Bryan <bryansanudo95@gmail.com>
2023-05-23 14:11:01 -0500

    save products to reudx store

--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -15,10 +15,14 @@ import { Link } from "react-router-dom";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import Loader from "@/components/loader/Loader";
 import Notiflix from "notiflix";
+import { useDispatch } from "react-redux";
+import { STORE_PRODUCTS } from "@/redux/slice/productSlice";
 
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getProducts();
@@ -40,6 +44,11 @@ const ViewProducts = () => {
         console.log(allProducts);
         setProducts(allProducts);
         setIsLoading(false);
+        dispatch(
+          STORE_PRODUCTS({
+            products: allProducts,
+          })
+        );
       });
     } catch (error) {
       setIsLoading(false);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,22 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "@/redux/slice/authSlice";
+import productReducer from "@/redux/slice/productSlice";
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
+
+/* const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+}); */
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  product: productReducer,
 });
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
 export default store;
